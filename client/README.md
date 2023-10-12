5caed75620d194a7f05be1500694130e82f15285
x.ande <ananfro@live.com>
2023-10-12 13:36:44 +1100

    resume page

--- client/README.md
+++ client/README.md
@@ -14,9 +14,6 @@ background >> https://www.vecteezy.com/photo/11043975-starry-night-sky-and-milky
 WHEN I click on a navigation title
 THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
 
-WHEN I am presented with the About Me section
-THEN I see a recent photo or avatar of the developer and a short bio about them
-
 WHEN I am presented with the Contact section
 THEN I see a contact form with fields for a name, an email address, and a message
 WHEN I move my cursor out of one of the form fields without entering text
--- client/src/views/ViewResume.jsx
+++ client/src/views/ViewResume.jsx
@@ -1,8 +1,38 @@
+// import resumeDoc from "../assets/resume1.docx";
+
 export default function PageResume() {
 
     return (
 
-        <>Resume</>
+        <>
+
+            <div id="wrapper">
+
+                <h1>Resume</h1>
+
+                <div id="content-container">
+
+                    <div id="p-container">
+                        <h2>Download Link</h2>
+                        <p>
+                            You can download my resume <a href="../assets/resume1.docx" download>here</a>.
+                        </p>
+                        <h2>Proficiencies</h2>
+                        <ul>
+                            <li>HTML, CSS, JavaScript</li>
+                            <li>React, Handlebars</li>
+                            <li>MongoDB, Mongoose, GraphQL</li>
+                            <li>MySQL, Sequelize</li>
+                        </ul>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </>
 
     );
 
