56da0420605675dbd0261238baafce26b8e0b8d8
x.ande <ananfro@live.com>
2023-10-12 21:04:51 +1100

    changed a to react-router-dom Link

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../styles/Navigation.css";
 
 export default function Header() {
@@ -7,11 +8,14 @@ export default function Header() {
         <nav>
 
             <span>
-
-                <a href="/about" id="nav-about">About Me</a>
+                <Link to="/about">About</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/resume">Resume</Link>
+                {/* <a href="/about" id="nav-about">About Me</a>
                 <a href="/portfolio" id="nav-portfolio">Portfolio</a>
                 <a href="/contact" id="nav-contact">Contact</a>
-                <a href="/resume" id="nav-resume">Resume</a>
+                <a href="/resume" id="nav-resume">Resume</a> */}
 
             </span>
 
